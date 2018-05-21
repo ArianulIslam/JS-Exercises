9bc915aa80ac8f08ca9ec1bc5044c31204f94be0
Arianul Islam <islamarianul@gmail.com>
2018-05-21 04:05:33 +0000

    task 1 2 3 4 completed

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-/* JavaScript code here */
+//task 1
+console.log("I am From External JS");
+
+//task 2
+
+function summmation(val) {
+  var sum=0;
+        val.forEach(function(value, index){
+          sum+=value;
+
+        });
+        return sum;
+        }
+
+        console.log(summmation([1,2,3,4,5]));
+     
+
+//Task 3
+
+function verifYEmail(email){
+  var emailstring = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  var isCorrect = emailstring.test(email);
+  return isCorrect;
+}
+
+console.log(verifYEmail("an.ariyan@yaoo.com"));
+
+
+//Task 4
+
+var library = [
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games',
+       readingStatus: false
+   }];
+
+function getReadingStatus(index){
+  return library[index].readingStatus;
+}
+
+console.log(getReadingStatus(2));
+
